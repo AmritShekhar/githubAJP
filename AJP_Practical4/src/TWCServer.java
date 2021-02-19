611a17ec64cd297c9de3b86c689b2e24a4204a2b
Amrit Shekhar Rana <amritshekharrana@gmail.com>
2021-02-19 14:30:33 +0530

    AJP Practical 4 committed

---
+++ AJP_Practical4/src/TWCServer.java
@@ -0,0 +1,28 @@
+import java.net.*;
+import java.io.*;
+public class TWCServer {
+	public static void main(String[] args) throws Exception {
+		
+		ServerSocket ss = new ServerSocket(24);
+		Socket s = ss.accept();
+		
+		System.out.println("I'm Ready to accept request!!");
+		
+		DataOutputStream writeOverBuffer = new DataOutputStream(s.getOutputStream());
+		DataInputStream readFromBuffer = new DataInputStream(s.getInputStream());
+		BufferedReader readFromKB = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str1 = "", str2 = "";
+		while(!str1.equals("See you!")) {
+			str1 = readFromBuffer.readUTF();
+			System.out.println("Client: " + str1);
+			str2 = readFromKB.readLine();
+			writeOverBuffer.writeUTF(str2);
+			writeOverBuffer.flush();
+			
+		}
+		readFromBuffer.close();
+		s.close();
+		ss.close();
+	}
+}
